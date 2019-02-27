bb9bbf8b87bf9d17a84c77f959d4fb03f072f7c3
Harsha Bhargav <hbhargav@users.noreply.github.com>
2019-02-27 17:47:29 +0530

    Update PrimeNumber.java

--- src/main/java/com/talfinder/assessment/PrimeNumber.java
+++ src/main/java/com/talfinder/assessment/PrimeNumber.java
@@ -1,19 +1,19 @@
 package com.talfinder.assessment;
 
 public class PrimeNumber {
-    
-    public boolean isPrime(Integer number) {
-        String spotBug = new String("Harsha Bhargav");
-        Integer temp = null;
-        boolean isPrime = true;
-        for(int i=2;i<=number/2;i++) {
-            temp=number%i;
-	        if(temp==0)
-	           {
-	               isPrime=false;
-	               break;
-	           }             
-        }
-        return isPrime;
+
+    public static boolean isPrime(Integer number) {
+        /*
+         * NOTE:
+         * 1. Write your solution within this method
+         * 2. Do not make changes to the Class Name, Method Name or Method Signature
+         * 3. Use the Run-Java command to run main method
+         * 4. Use the BuildAndRunTests command to evaluate your solution
+         */
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Is Prime : " + isPrime(9));
     }
 }
